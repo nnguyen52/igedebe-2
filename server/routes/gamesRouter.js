ff36ea0f91726054928e4713e59b5e4148b546d5
jer_ng <coh.jr11@gmail.com>
2021-11-15 21:15:19 -0800

    deploy server

---
+++ server/routes/gamesRouter.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const gamesCtrl = require('../controllers/gamesCtrl');
+router.post('/getPre500games', gamesCtrl.getPre500games);
+router.post('/justReleasedGames', gamesCtrl.getJustReleasedGames);
+router.post('/comingSoonGames', gamesCtrl.comingSoonGames);
+router.post('/mostAnticipatedGames', gamesCtrl.mostAnticipatedGames);
+router.post('/getGamesDetails/:id', gamesCtrl.getGamesDetails);
+router.get('/getJustReleasedGamesPagination/:offset', gamesCtrl.getJustReleasedGamesPagination);
+router.get(
+  '/getMostAnticipatedGamesPagination/:offset',
+  gamesCtrl.getMostAnticipatedGamesPagination
+);
+router.get('/getJustComingSoonGamesPagination/:offset', gamesCtrl.getJustComingSoonGamesPagination);
+router.post('/searchGames/:query', gamesCtrl.searchGame);
+module.exports = router;
