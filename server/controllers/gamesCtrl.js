1b0916f2a5534f9ff6229839867561df8ebe3c40
jer_ng <coh.jr11@gmail.com>
2021-11-15 23:50:27 -0800

    get 2 games for testing

--- server/controllers/gamesCtrl.js
+++ server/controllers/gamesCtrl.js
@@ -11,7 +11,7 @@ const gamesCtrl = {
     try {
       const response = await apiConfig(
         '/games',
-        `fields name, first_release_date , hypes; where hypes > 10 ; sort first_release_date desc ; limit 500;`
+        `fields name, first_release_date , hypes; where hypes > 10 ; sort first_release_date desc ; limit 2;`
       );
 
       const filtered = filterDuplicatesByName_forGames(response.data);
