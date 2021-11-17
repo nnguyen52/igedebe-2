8e2e2cd8de50e829fbe9c7d16a8f1db600d8c09f
jer_ng <coh.jr11@gmail.com>
2021-11-17 01:08:49 -0800

    testing getStaticProps without []

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -198,7 +198,7 @@ export async function getStaticProps({ params }) {
   const res = await axios.post(`${apiURL}/api/getGamesDetails/${params.id}`);
   return {
     props: {
-      gameWithDetails: res.data.data || [],
+      gameWithDetails: res.data.data,
     },
     revalidate: (60 * 60 * 1000) / 4, //refresh detailed game every 1/4 day
   };
