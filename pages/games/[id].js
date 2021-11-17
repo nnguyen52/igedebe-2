23d7097f1098d959ad7965a2c76debe5bbb0026d
jer_ng <coh.jr11@gmail.com>
2021-11-16 23:32:58 -0800

    testing /games/[id] with revalidate 1

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -195,7 +195,7 @@ export async function getStaticProps({ params }) {
     props: {
       gameWithDetails: res.data.data || [],
     },
-    revalidate: (60 * 60 * 1000) / 4, //refresh detailed game every 1/4 day
+    revalidate: 1, //refresh detailed game every 1/4 day
   };
 }
 export async function getStaticPaths() {
